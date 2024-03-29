fb6853df77fbc007c3092de6f9dc7ee028b81e9c
ChrisChen <chrischen@going.cloud>
2024-03-29 10:26:59 +0800

    add i18next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import i18nextConfig from './next-i18next.config.js'
+
+const {i18n} = i18nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n
 };
 
 export default nextConfig;
